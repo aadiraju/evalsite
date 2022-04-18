eff14d499b2c91395d80ff1e12696179a999e9c6
Abhineeth Adiraju <abhineeth00@gmail.com>
2022-04-18 00:19:33 -0700

    changed sizing and question display

--- evaluation/admin.py
+++ evaluation/admin.py
@@ -4,9 +4,20 @@ from django.contrib import admin
 # Register your models here.
 from .models import UserConsent, UserFeedback, Video, UserVideoJunction, Question, TrialData
 
+
+class UserVideoJunctionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'video', 'rating')
+    list_filter = ('user', 'video', 'rating')
+    search_fields = ('user', 'video')
+
+
+class TrialDataAdmin(admin.ModelAdmin):
+    list_filter = ('user', 'alpha_value', 'trial_number')
+
+
 admin.site.register(Video)
-admin.site.register(UserVideoJunction)
+admin.site.register(UserVideoJunction, UserVideoJunctionAdmin)
 admin.site.register(Question)
-admin.site.register(TrialData)
+admin.site.register(TrialData, TrialDataAdmin)
 admin.site.register(UserConsent)
 admin.site.register(UserFeedback)
